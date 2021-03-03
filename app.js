3eed1c887a63501c5217d3a6f7cac0b6c8f468cf
Austin <acbrennan101@gmail.com>
2021-03-03 09:23:56 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//movement animation to happen
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+//moving animation event
+container.addEventListener('mousemove', (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+    
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg`;
+});
